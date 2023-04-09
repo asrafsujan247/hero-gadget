aabb79d1146a8ab76db4344b9df3d4e810aadac5
asrafsujan247 <asraf.sujan247@gmail.com>
2023-04-10 05:34:39 +0600

    added home about header and footer compo  and set up react router and app home about routes

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <>
+      <Header></Header>
+      <div className="min-h-[calc(100vh-157px)]">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default App;
