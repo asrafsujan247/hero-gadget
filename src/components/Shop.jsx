7ec9a778f70b4de44103e59a84e2c129341191bb
asrafsujan247 <asraf.sujan247@gmail.com>
2023-04-10 10:59:01 +0600

    showed the data in productCard

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import ProductCard from "./Cards/ProductCard";
 
 const Shop = () => {
   const productData = useLoaderData();
-  console.log(productData);
-  return <div>shop compo :{productData.length}</div>;
+
+  //   cart button handler
+  const handleAddToCart = (id) => {
+    console.log(id);
+  };
+
+  return (
+    <div className="product-container">
+      {productData.map((product) => (
+        <ProductCard key={product.id} product={product}></ProductCard>
+      ))}
+    </div>
+  );
 };
 
 export default Shop;
