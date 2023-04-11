06f5f0ceb0678eb70980b87f4d38c8669b91b49a
asrafsujan247 <asraf.sujan247@gmail.com>
2023-04-11 11:22:20 +0600

    added modal feature with session storage

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import { Outlet, useLoaderData } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { createContext, useState } from "react";
+import Modal from "./components/Modal";
 
 export const ProductContext = createContext([]);
 export const CartContext = createContext([]);
 
 const App = () => {
+  let [isOpen, setIsOpen] = useState(false);
   const { cartArray, products } = useLoaderData();
   const [cart, setCart] = useState(cartArray);
+
+  const cartAlert = sessionStorage.getItem("alert");
+  if (cartArray.length > 0 && cartAlert !== "true") {
+    setIsOpen(true);
+    sessionStorage.setItem("alert", true);
+  }
 
   return (
     <ProductContext.Provider value={products}>
@@ -18,6 +26,7 @@ const App = () => {
           <Outlet></Outlet>
         </div>
         <Footer></Footer>
+        <Modal isOpen={isOpen} setIsOpen={setIsOpen}></Modal>
       </CartContext.Provider>
     </ProductContext.Provider>
   );
